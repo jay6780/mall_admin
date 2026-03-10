0330117289ca63e35ae7ac7c3e650e45877c8ab1
Ar-jay <arjayurbina43@gmail.com>
2026-03-11 01:16:04 +0800

    first

---
+++ config/dev.env.js
@@ -0,0 +1,8 @@
+'use strict'
+const merge = require('webpack-merge')
+const prodEnv = require('./prod.env')
+
+module.exports = merge(prodEnv, {
+  NODE_ENV: '"development"',
+  BASE_API: '"http://144.172.116.215:8080/"'
+})
